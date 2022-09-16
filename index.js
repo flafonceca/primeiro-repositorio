f8de7d4693f9e7c62800aa0dc3779dda06a175e6
flafonceca <flaziff@gmail.com>
2022-09-16 16:54:08 -0300

    operadores de incremento e decremento

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello world")
+console.log(5)
+console.log(43.76)
+console.log(true)
+
+let mensagem= "minha mensagem"
+console.log(mensagem)
+mensagem="novo texto"
+console.log(mensagem)
---
+++ operadores_de_incremento.js
@@ -0,0 +1,9 @@
+alert("Código Funcionando!")
+const nomeUsuario = prompt("Olá, usuário, qual o seu nome?")
+
+let num = Number(prompt("Olá," + nomeUsuario + ". Digite algum número, por favor"))
+//const sucessor=num++ // = num +1
+//alert("O sucessor de " + num + " é " + sucessor)
+
+console.log(num++)
+console.log(num)
